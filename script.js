89f275536d8c0a1c4eeacc734bb8cdd5f6e2cc38
Joshua Steed <56124543+0xjoshva@users.noreply.github.com>
2022-06-17 16:13:28 +0200

    spelling changes

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const celciusInput = document.getElementById("celcius");
+const celsiusInput = document.getElementById("celsius");
 const fahrenheitInput = document.getElementById("fahrenheit");
 
-// console.log(celciusInput)
-// console.log(fahrenheitInput)
-
 const inputs = document.getElementsByClassName("input");
-// console.log(inputs)
 
 for (let i = 0; i < inputs.length; i++) {
   let input = inputs[i];
@@ -14,12 +10,12 @@ for (let i = 0; i < inputs.length; i++) {
     let value = parseFloat(e.target.value);
 
     switch (e.target.name) {
-      case "celcius":
+      case "celsius":
         fahrenheitInput.value = value * 1.8 + 32;
         kelvinInput.value = value + 273.15;
         break;
       case "fahrenheit":
-        celciusInput.value = (value - 32) / 1.8;
+        celsiusInput.value = (value - 32) / 1.8;
         kelvinInput.value = (value - 32) / 1.8 + 273.15;
         break;
     }
